342d4939431341e387b9a46682cd3fe83d22decd
chrnadig <cnadig@mac.com>
2021-11-08 21:58:59 +0100

    Added macOS Framework as a target

---
+++ FT8Framework/FT8Framework.h
@@ -0,0 +1,19 @@
+//
+//  FT8Framework.h
+//  FT8Framework
+//
+//  Created by Christoph Nadig on 08.11.21.
+//
+
+#import <Foundation/Foundation.h>
+#import <FT8Framework/ft8.h"
+
+//! Project version number for FT8Framework.
+FOUNDATION_EXPORT double FT8FrameworkVersionNumber;
+
+//! Project version string for FT8Framework.
+FOUNDATION_EXPORT const unsigned char FT8FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FT8Framework/PublicHeader.h>
+
+
